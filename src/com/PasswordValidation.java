a20b84b8314bbd5222121e845a32f953a5dbd4e5
unknown <dhirajmurarka@outlook.com>
2022-12-03 21:35:21 +0530

    Added appplyFeature2 boolean variable so can switch to feature 2

--- src/com/PasswordValidation.java
+++ src/com/PasswordValidation.java
@@ -2,12 +2,30 @@ package com;
 
 public class PasswordValidation {
 
-	public static void isValid(String password) throws InvalidPasswordException {
-		if (password == null) {
-			throw new InvalidPasswordException(2);
+	public static void isValid(String password, boolean appplyFeature2) throws InvalidPasswordException
+	{
+		int rank = 5;
+		if (password == null)
+		{
+			if(appplyFeature2)
+			{
+				rank--;
+			}
+			else
+			{
+				throw new InvalidPasswordException(2);
+			}
 		}
-		if (password.length() <= 8) {
-			throw new InvalidPasswordException(1);
+		if (password.length() <= 8)
+		{
+			if(appplyFeature2)
+			{
+				rank--;
+			}
+			else
+			{
+				throw new InvalidPasswordException(1);
+			}
 		}
 
 		int count = 0;
@@ -17,19 +35,31 @@ public class PasswordValidation {
 			}
 		}
 		if (count == 0) {
-			throw new InvalidPasswordException(3);
+			if(appplyFeature2)
+			{
+				rank--;
+			}
+			else
+			{
+				throw new InvalidPasswordException(3);
+			}
 		}
 
 		count = 0;
 		for (int i = 90; i <= 122; i++) {
-			String str1 = Character.toString((char) i);
-
-			if (password.contains(str1)) {
+			if (password.contains(Character.toString((char) i))) {
 				count = 1;
 			}
 		}
 		if (count == 0) {
-			throw new InvalidPasswordException(4);
+			if(appplyFeature2)
+			{
+				rank--;
+			}
+			else
+			{
+				throw new InvalidPasswordException(4);
+			}
 		}
 
 		count = 0;
@@ -39,9 +69,15 @@ public class PasswordValidation {
 			}
 		}
 		if (count == 0) {
-			throw new InvalidPasswordException(5);
+			if(appplyFeature2)
+			{
+				rank--;
+			}
+			else
+			{
+				throw new InvalidPasswordException(5);
+			}
 		}
-
 	}
 
 	public static void main(String[] args) {
--- test/com/PasswordTest.java
+++ test/com/PasswordTest.java
@@ -10,7 +10,7 @@ public class PasswordTest {
 		PasswordValidation pv = new PasswordValidation();
 		String password = "Abcd";
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
 		}
@@ -18,7 +18,7 @@ public class PasswordTest {
 		//Case 1.2
 		password = null;
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
 		}
@@ -26,7 +26,7 @@ public class PasswordTest {
 		//Case 1.3
 		password = "abcdefghijk";
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
 		}
@@ -34,7 +34,7 @@ public class PasswordTest {
 		//Case 1.4
 		password = "ABCDEFGHIJK";
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
 		}
@@ -42,14 +42,14 @@ public class PasswordTest {
 		//Case 1.5
 		password = "AbcdEFGSFGHHFS";
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
 		}
 
 		password = "Abcdefgh1";
 		try {
-			pv.isValid(password);
+			pv.isValid(password,false);
 			System.out.println("Password is OK");
 		} catch (InvalidPasswordException e) {
 			System.out.println(e.printMessage());
